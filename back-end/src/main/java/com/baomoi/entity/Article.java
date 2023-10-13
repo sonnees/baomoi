ae4bd30082bb69447c964e15dfb0ae1687004bbc
sonnees <sson12131415@gmail.com>
2023-10-13 22:50:22 +0700

    junit base publisher

--- back-end/src/main/java/com/baomoi/entity/Article.java
+++ back-end/src/main/java/com/baomoi/entity/Article.java
@@ -9,43 +9,83 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
-@Getter @Setter @ToString
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "article")
-public class Article {
+public class Article implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    private UUID id = UUID.randomUUID();
 
-    @Column(columnDefinition = "varchar(200)")
-    private String title;
+    @Column(columnDefinition = "varchar(200)", nullable = false)
+    private String title ="";
 
-    @Column(name = "post-time")
+    @Column(name = "post-time", nullable = false)
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    private LocalDateTime postTime;
+    private LocalDateTime postTime = LocalDateTime.now();
 
-    @Column(columnDefinition = "varchar(500)")
-    private String summary;
+    @Column(columnDefinition = "varchar(500)", nullable = false)
+    private String summary="";
 
-    @Column(columnDefinition = "varchar(5000)")
-    private String contend;
+    @Column(columnDefinition = "varchar(5000)", nullable = false)
+    private String contend="";
 
     @Enumerated
-    private Category category;
+    @Column(nullable = false)
+    private Category category = Category.BONGDA;
 
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "article")
-    private List<ImageArticle> imageArticles;
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "article", cascade = CascadeType.REMOVE)
+    private List<ImageArticle> imageArticles = new ArrayList<>();
 
-    @ManyToOne
-    @JoinColumn(name = "publisher_id")
-    private Publisher publisher;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "publisher_id", nullable = false)
+    private Publisher publisher = new Publisher();
 
+    public Article(String title, LocalDateTime postTime, String summary, String contend, Category category, List<ImageArticle> imageArticles, Publisher publisher) {
+        this.title = title;
+        this.postTime = postTime;
+        this.summary = summary;
+        this.contend = contend;
+        this.category = category;
+        this.imageArticles = imageArticles;
+        this.publisher = publisher;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(id, article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", postTime=" + postTime +
+                ", summary='" + summary + '\'' +
+                ", contend='" + contend + '\'' +
+                ", category=" + category +
+                ", imageArticles=" + imageArticles +
+//                ", publisher=" + publisher +
+                '}';
+    }
 }
--- back-end/src/main/java/com/baomoi/entity/ImageArticle.java
+++ back-end/src/main/java/com/baomoi/entity/ImageArticle.java
@@ -3,26 +3,56 @@ package com.baomoi.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
-@Getter @Setter @ToString
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "image_article")
-public class ImageArticle {
+public class ImageArticle implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    private UUID id = UUID.randomUUID();
 
-    @Column(columnDefinition = "varchar(255)")
+    @Column(columnDefinition = "varchar(255)",nullable = false)
     private String title;
 
-    @Column(columnDefinition = "varchar(255)")
+    @Column(columnDefinition = "varchar(255)",nullable = false)
     private String imageURL;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "article_id")
     private Article article;
 
+    public ImageArticle(String title, String imageURL, Article article) {
+        this.title = title;
+        this.imageURL = imageURL;
+        this.article = article;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ImageArticle that = (ImageArticle) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ImageArticle{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", imageURL='" + imageURL + '\'' +
+//                ", article=" + article +
+                '}';
+    }
 }
